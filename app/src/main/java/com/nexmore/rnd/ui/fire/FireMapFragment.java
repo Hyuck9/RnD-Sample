761e8c636628ca8e44d2af261df5f9d15d9d276f
Hyuck9 <lhg1304@gmail.com>
2019-10-11 18:22:03 +0900

    화재 대피로 등등등 추가

--- app/src/main/java/com/nexmore/rnd/ui/fire/FireMapFragment.java
+++ app/src/main/java/com/nexmore/rnd/ui/fire/FireMapFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.chrisbanes.photoview.PhotoView;
 import com.nexmore.rnd.R;
 
 public class FireMapFragment extends Fragment {
@@ -25,7 +26,10 @@ public class FireMapFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_firemap, container, false);
+        View root = inflater.inflate(R.layout.fragment_firemap, container, false);
+        PhotoView photoView = root.findViewById(R.id.photoView);
+        photoView.setImageResource(R.drawable.floor_four);
+        return root;
     }
 
     @Override
--- app/src/main/java/com/nexmore/rnd/ui/home/HomeFragment.java
+++ app/src/main/java/com/nexmore/rnd/ui/home/HomeFragment.java
@@ -5,14 +5,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import com.nexmore.rnd.R;
+
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
@@ -22,13 +27,27 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+
+        root.findViewById(R.id.cardView).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_home_to_nav_location);
             }
         });
+
+        root.findViewById(R.id.cardView2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_home_to_fireMapFragment);
+            }
+        });
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
