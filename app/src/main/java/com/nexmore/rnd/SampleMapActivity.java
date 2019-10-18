58a310f108db8cc4035782689809b09bb80a7022
Hyuck9 <lhg1304@gmail.com>
2019-10-18 18:40:47 +0900

    테스트용 Toast Message

--- app/src/main/java/com/nexmore/rnd/SampleMapActivity.java
+++ app/src/main/java/com/nexmore/rnd/SampleMapActivity.java
@@ -5,6 +5,7 @@ import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.nexmore.rnd.databinding.ActivitySampleMapBinding;
 
@@ -47,7 +48,7 @@ public class SampleMapActivity extends AppCompatActivity implements MapView.Curr
      */
     @Override
     public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
-        // 단말의 현위치 좌표값을 통보받을 수 있다.
+        Toast.makeText(this, "단말의 현위치 좌표값을 통보받을 수 있다.", Toast.LENGTH_SHORT).show();
         //
         // MapView.setCurrentLocationTrackingMode 메소드를 통해
         // 사용자 현위치 트래킹 기능이 켜진 경우(CurrentLocationTrackingMode.TrackingModeOnWithoutHeading, CurrentLocationTrackingMode.TrackingModeOnWithHeading)
@@ -55,7 +56,7 @@ public class SampleMapActivity extends AppCompatActivity implements MapView.Curr
     }
     @Override
     public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
-        // 단말의 방향(Heading) 각도값을 통보받을 수 있다.
+        Toast.makeText(this, "단말의 방향(Heading) 각도값을 통보받을 수 있다.", Toast.LENGTH_SHORT).show();
         //
         // MapView.setCurrentLocationTrackingMode 메소드를 통해
         // 사용자 현위치 트래킹과 나침반 모드가 켜진 경우(CurrentLocationTrackingMode.TrackingModeOnWithHeading)
@@ -63,11 +64,11 @@ public class SampleMapActivity extends AppCompatActivity implements MapView.Curr
     }
     @Override
     public void onCurrentLocationUpdateFailed(MapView mapView) {
-        // 현위치 갱신 작업에 실패한 경우 호출된다.
+        Toast.makeText(this, "현위치 갱신 작업에 실패한 경우 호출된다.", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onCurrentLocationUpdateCancelled(MapView mapView) {
-        // 현위치 트랙킹 기능이 사용자에 의해 취소된 경우 호출된다.
+        Toast.makeText(this, "현위치 트랙킹 기능이 사용자에 의해 취소된 경우 호출된다.", Toast.LENGTH_SHORT).show();
         // 처음 현위치를 찾는 동안에 현위치를 찾는 중이라는 Alert Dialog 인터페이스가 사용자에게 노출된다.
         // 첫 현위치를 찾기전에 사용자가 취소 버튼을 누른 경우 호출 된다.
     }
@@ -83,11 +84,11 @@ public class SampleMapActivity extends AppCompatActivity implements MapView.Curr
      */
     @Override
     public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
-        // 주소를 찾은 경우 호출된다.
+        Toast.makeText(this, "주소를 찾은 경우 호출된다.", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
-        // Reverse Geo-Coding 서비스 호출에 실패한 경우 호출된다.
+        Toast.makeText(this, "Reverse Geo-Coding 서비스 호출에 실패한 경우 호출된다.", Toast.LENGTH_SHORT).show();
     }
     /* MapReverseGeoCoder.ReverseGeoCodingResultListener End */
 
@@ -101,7 +102,7 @@ public class SampleMapActivity extends AppCompatActivity implements MapView.Curr
      */
     @Override
     public void onMapViewInitialized(MapView mapView) {
-        // MapView가 사용가능 한 상태가 되었음을 알려준다.
+        Toast.makeText(this, "MapView가 사용가능 한 상태가 되었음을 알려준다.", Toast.LENGTH_SHORT).show();
         // onMapViewInitialized()가 호출된 이후에 MapView 객체가 제공하는 지도 조작 API들을 사용할 수 있다.
 
         mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(35.87137603, 128.601806640625), 5, true);   // 대구광역시청
@@ -111,35 +112,35 @@ public class SampleMapActivity extends AppCompatActivity implements MapView.Curr
     }
     @Override
     public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
-        // 지도 중심 좌표가 이동한 경우 호출된다.
+        Toast.makeText(this, "지도 중심 좌표가 이동한 경우 호출된다.", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onMapViewZoomLevelChanged(MapView mapView, int i) {
-        // 지도 확대/축소 레벨이 변경된 경우 호출된다.
+        Toast.makeText(this, "지도 확대/축소 레벨이 변경된 경우 호출된다.", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
-        // 사용자가 지도 위를 터치한 경우 호출된다.
+        Toast.makeText(this, "사용자가 지도 위를 터치한 경우 호출된다.", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
-        // 사용자가 지도 위 한 지점을 더블 터치한 경우 호출된다.
+        Toast.makeText(this, "사용자가 지도 위 한 지점을 더블 터치한 경우 호출된다.", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
-        // 사용자가 지도 위 한 지점을 길게 누른 경우(long press) 호출된다.
+        Toast.makeText(this, "사용자가 지도 위 한 지점을 길게 누른 경우(long press) 호출된다.", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
-        // 사용자가 지도 드래그를 시작한 경우 호출된다.
+        Toast.makeText(this, "사용자가 지도 드래그를 시작한 경우 호출된다.", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
-        // 사용자가 지도 드래그를 끝낸 경우 호출된다.
+        Toast.makeText(this, "사용자가 지도 드래그를 끝낸 경우 호출된다.", Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
-        // 지도의 이동이 완료된 경우 호출된다.
+        Toast.makeText(this, "지도의 이동이 완료된 경우 호출된다.", Toast.LENGTH_SHORT).show();
 
         MapPoint.GeoCoordinate mapPointGeo = mapView.getMapCenterPoint().getMapPointGeoCoord();
         viewModel.setLatLon(mapPointGeo.latitude, mapPointGeo.longitude);
@@ -157,16 +158,18 @@ public class SampleMapActivity extends AppCompatActivity implements MapView.Curr
      */
     @Override
     public void onPOIItemSelected(MapView mapView, MapPOIItem mapPOIItem) {
-        // 단말 사용자가 POI Item을 선택한 경우 호출된다.
+        Toast.makeText(this, "단말 사용자가 POI Item을 선택한 경우 호출된다.", Toast.LENGTH_SHORT).show();
         // 사용자가 MapView 에 등록된 POI Item 아이콘(마커)를 터치한 경우 호출된다.
     }
     @Override
     public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem) { /* Deprecated */ }
     @Override
-    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) { /* 설명 없음 */ }
+    public void onCalloutBalloonOfPOIItemTouched(MapView mapView, MapPOIItem mapPOIItem, MapPOIItem.CalloutBalloonButtonType calloutBalloonButtonType) {
+        Toast.makeText(this, "onCalloutBalloonOfPOIItemTouched 호출", Toast.LENGTH_SHORT).show();
+    }
     @Override
     public void onDraggablePOIItemMoved(MapView mapView, MapPOIItem mapPOIItem, MapPoint mapPoint) {
-        // 단말 사용자가 길게 누른 후(long press) 끌어서(dragging) 위치 이동이 가능한 POI Item의 위치를 이동시킨 경우 호출된다.
+        Toast.makeText(this, "단말 사용자가 길게 누른 후(long press) 끌어서(dragging) 위치 이동이 가능한 POI Item의 위치를 이동시킨 경우 호출된다.", Toast.LENGTH_SHORT).show();
         // 이동가능한 POI Item을 Draggable POI Item 이라 한다.
     }
     /* MapView.POIItemEventListener End */
