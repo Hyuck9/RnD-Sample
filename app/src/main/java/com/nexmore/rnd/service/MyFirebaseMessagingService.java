f28d788798dc7f6a2658f9dd5a1d2a2f9c61d313
Hyuck9 <lhg1304@gmail.com>
2019-10-23 12:27:54 +0900

    FCM 기능 추가 하는 중

---
+++ app/src/main/java/com/nexmore/rnd/service/MyFirebaseMessagingService.java
@@ -0,0 +1,9 @@
+package com.nexmore.rnd.service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+}
